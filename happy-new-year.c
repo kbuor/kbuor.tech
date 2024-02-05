d2529c615133c62a3654e514fc940d87f9963923
kbuor <77895173+kbuor@users.noreply.github.com>
2024-02-05 21:37:38 +0700

    Create happy-new-year.c

---
+++ happy-new-year.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Include standard input and output library
+
+// Main function: Entry point of the program
+int main(void) {
+    // Print the greeting message to the console
+    printf("Happy Lunar New Year\n");
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
